3f6d95cbbc1e91fb820c346001e97e4fec7f2241
wawanabdullah1704-bit <wawanabdullah1704@gmail.com>
2026-01-22 11:36:06 +0800

    tak tahu

--- W2/bmi-calculator.php
+++ W2/bmi-calculator.php
@@ -8,14 +8,25 @@
 <body>
     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
         <label for="">
-            Height (cm)
+            Height (m)
         </label>
-        <input name="height" type="number">
+        <input name="heightVal" type="number">
         <label for="">
             Weight (kg)
         </label>
-        <input name="weight" type="number">
+        <input name="weightVal" type="number">
         <input type="submit" value="Calculate">
     </form>
 </body>
+<?php
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+    $height = $_POST['heightVal'];
+    $weight = $_POST['weightVal'];
+
+    $bmi = $weight/(($height/100)* ($height/100));
+    }
+?>
+<?php
+echo $bmi;
+?>
 </html>
