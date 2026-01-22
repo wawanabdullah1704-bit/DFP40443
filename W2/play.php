1e51c36e22cd2b9e9cae0370907df9d59bff4ea0
wawanabdullah1704-bit <wawanabdullah1704@gmail.com>
2026-01-22 10:44:44 +0800

    third commit

--- W2/play.php
+++ W2/play.php
@@ -2,7 +2,7 @@
 $name = "Wawan";
 echo "<h1>Hello World $name</h1>";
 echo 'This is a full stack class'.$name;
-echo "<h1>my name is Wawan Bin Abdullah<h1>"
-echo "<h2>I am from JTMK<h2>"
-echo "<h3>and my PA name is Miss Athirah<h3>"
+echo "<h1>my name is Wawan Bin Abdullah<h1>";
+echo "<h2>I am from JTMK<h2>";
+echo "<h3>and my PA name is Miss Athirah<h3>";
 ?>
